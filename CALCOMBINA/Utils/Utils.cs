af57d19f7a70d05c4089209c2152614cc9286b22
Marcus Saraiva <marcus.saraiva@cienciasnauticas.org.br>
2022-08-09 12:56:12 -0300

    :pen: Implementando combinação e permutação.

---
+++ CALCOMBINA/Utils/Utils.cs
@@ -0,0 +1,20 @@
+﻿namespace CALCOMBINA.Utils
+{
+    public class Utils
+    {
+        public static long CalcularFatorial(long variavel)
+        {
+            long fatorial;
+
+            if (variavel <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                fatorial = variavel * CalcularFatorial(variavel - 1);
+                return fatorial;
+            }
+        }
+    }
+}
